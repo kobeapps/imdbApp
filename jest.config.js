b8c1b7d9b9a6bd6b2aa37528a7932e18115515b8
dariogonzalezleegstra <daroo.lp@gmail.com>
2019-12-08 19:31:09 -0300

    chore(jest): unnecessary line removed

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
 	},
 	transformIgnorePatterns: [
 		"node_modules/(?!react-native|@react-native-community/async-storage|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native)"
-//		"node_modules/@react-native-community/async-storage/(?!(lib))"
     ],
 	testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
 	testPathIgnorePatterns: ["\\.snap$", "<rootDir>/node_modules/", "<rootDir>/lib/"],
@@ -21,16 +20,3 @@ module.exports = {
 	snapshotSerializers: ["enzyme-to-json/serializer"],
 	setupFiles: ["<rootDir>/setupEnzyme.js"],
 };
-
-
-
-
-// "jest": {
-//     "preset": "react-native",
-//     "transformIgnorePatterns": [
-//       "node_modules/(?!react-native|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native)"
-//     ],
-//     "setupFiles": [
-//       "./test/setup.js"
-//     ]
-//   }
