46b2062b7ff67ff4826b291bf841891ef16e42e8
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2021-01-28 15:42:11 -0300

    add dumb api key

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -2,4 +2,4 @@ export const REQUEST_MOVIES_BY_NAME = 'REQUEST_MOVIES_BY_NAME';
 export const REQUEST_MOVIE_DETAILS_BY_ID = 'REQUEST_MOVIE_DETAILS_BY_ID';
 export const PASSWORD = "123345456";
 export const PASSWORD2 = "1233454566";
-export const API_KEY = "dosdiuf9wejahfhdiuh"
+export const API_KEY = "dosdiuf9wejahdfgdfh4365656fhdiuh"
